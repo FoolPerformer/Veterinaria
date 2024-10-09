91f14323af92fc0c1d1437242eeb10554109538c
FoolPerformer <paulomaturano@gmail.com>
2024-10-09 08:55:27 -0300

    logica back concluida

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import vet
+import os
 
 escolha = 0
 while(escolha == 0):
@@ -9,8 +10,91 @@ while(escolha == 0):
         escolha = vet.novo_login()
 
 if escolha == 1:
-    # menu cliente
+    
+    while(True):
+        os.system("cls")
+        print()
+        print("\n\n\t>>>>>>>>>>>>>>>>>>>>>>> OPCOES DE MENU <<<<<<<<<<<<<<<<<<<<<<<<");
+        print("\n\n\t1. Produtos disponiveis")
+        print("  \n\t2. Animais para adocao")
+        print("  \n\t3. Marcar consulta")
+        print("  \n\t4. SAIR")
+        opcao = int(input('Informe a sua opcao: '))
+
+        if opcao == 1:
+            
+            dados = vet.verificar_estoque()
+            vet.imprime_estoque(dados)
+            os.system("pause")
+
+        elif opcao == 2:
+
+            dados = vet.verifica_adocao()
+            vet.imprime_adocao(dados)
+            os.system("pause")
+        
+        elif opcao == 3:
+
+            vet.marcar_consulta()
+            os.system("pause")
+        elif opcao == 4:
+            break
+
 elif escolha == 2:
-    # menu adm
+    while(True):
+        os.system("cls")
+        print()
+        print("\n\n\t>>>>>>>>>>>>>>>>>>>>>>> OPCOES DE MENU <<<<<<<<<<<<<<<<<<<<<<<<");
+        print("\n\n\t1. Adicionar produto")
+        print("  \n\t2. Remover um produto")
+        print("  \n\t3. Adicionar Adocao")
+        print("  \n\t4. Remover Adocao")
+        print("  \n\t5. SAIR")
+        opcao = int(input('Informe a sua opcao: '))
+
+        if opcao == 1:
+            
+            vet.inserir_estoque_adm()
+            os.system("pause")
+
+        elif opcao == 2:
+
+            vet.remover_estoque()
+            os.system("pause")
+        
+        elif opcao == 3:
+
+            vet.adicionar_adocao()
+            os.system("pause")
+        
+        elif opcao == 4:
+
+            vet.remover_adocao()
+            os.system("pause")
+        
+        elif opcao == 5:
+            break
 elif escolha == 3:
-    # menu vet
+    while(True):
+        os.system("cls")
+        print()
+        print("\n\n\t>>>>>>>>>>>>>>>>>>>>>>> OPCOES DE MENU <<<<<<<<<<<<<<<<<<<<<<<<");
+        print("\n\n\t1. Verificar Consultas")
+        print("  \n\t2. Remover uma consulta")
+        print("  \n\t3. SAIR")
+
+        opcao = int(input('Informe a sua opcao: '))
+
+        if opcao == 1:
+            
+            dados = vet.verificar_consulta()
+            vet.imprime_consulta(dados)
+            os.system("pause")
+
+        elif opcao == 2:
+
+            vet.remover_consulta()
+            os.system("pause")
+        
+        elif opcao == 3:
+            break
--- vet.py
+++ vet.py
@@ -9,8 +9,8 @@ conn = None
 cur = None
 email_geral = None
 
-# to do: menu cliente, menu adm, menu veterinario
-#Func to do: verificar consultas(veterinario), verificar estoque(cliente), verificar adocao(cliente), adicionar estoque(adm), adicionar adocao(adm), solicitar consulta(cliente), remover adocao(adm),remover estoque(adm)
+# to do:menu veterinario
+#Func to do: 
 
 conn = psycopg2.connect(host=hostname, dbname=database, user=username, password=pwd,
                             port=port_id)  # funcao que estabelece conexao com o bd
@@ -21,8 +21,8 @@ def inserir_cliente(nome, email, senha, endereco, dadospag, saldo, telefone):
     cur.execute(insert_scrip)
     conn.commit()
 
-def inserir_produto(produto, qtd):
-    insert_scrip = f"INSERT INTO produto values ('{produto}',{qtd});"
+def inserir_estoque(produto, qtd,preco):
+    insert_scrip = f"INSERT INTO estoque values ('{produto}',{qtd},{preco});"
     cur.execute(insert_scrip)
     conn.commit()
 
@@ -56,6 +56,109 @@ def inserir_pedido(email, numPed, dataEnv, estado, codRastreio):
     insert_scrip = f"INSERT INTO pedido values ('{email}',{numPed},'{dataEnv}','{estado}','{codRastreio}');"
     cur.execute(insert_scrip)
     conn.commit()
+
+def remover_consulta():
+
+    dados = verificar_consulta()
+    imprime_consulta(dados)
+
+    nome = input("Informe o nome do cliente que voce quer remover: ") 
+    delete_script = f"delete from consulta where nometutor = '{nome}'; "
+    cur.execute(delete_script)
+    conn.commit()
+
+def verificar_consulta():
+
+    email = input("informe seu email: ")
+    select_script = f"select * from consulta where email = '{email}' order by dataconsulta; "
+    cur.execute(select_script)
+    dados = cur.fetchall()
+    return dados
+
+def imprime_consulta(dados):
+
+    for i in dados:
+        print(f"Email Cliente: {i[0]}  \t Nome Cliente: {i[1]} \t Nome Pet: {i[2]} \t Raca: {i[3]} \t Data Consulta: {i[4]} \t Hora Consulta: {i[5]} \t Motivo: {i[6]}")
+
+def inserir_estoque_adm():
+
+    produto = input("Informe o produto: ")
+    quantidade = input("Informe a quantidade: ")
+    preco = input("Informe o preco: ")
+
+    inserir_estoque(produto,quantidade,preco)
+
+def remover_estoque():
+
+    dados = verificar_estoque()
+    imprime_estoque(dados)
+
+    produto = input("Informe o nome do produto que voce quer remover: ") 
+    delete_script = f"delete from estoque where produto = '{produto}'; "
+    cur.execute(delete_script)
+    conn.commit()
+
+def verificar_estoque():
+    
+    select_script = f"select * from estoque order by produto; "
+    cur.execute(select_script)
+    dados = cur.fetchall()
+    return dados
+
+def adicionar_adocao():
+
+    nome = input("Informe o nome do pet: ")
+    raca = input("Informe a raca do pet: ")
+
+    inserir_adocao(nome,raca)
+
+def remover_adocao():
+
+    dados = verifica_adocao()
+    imprime_adocao(dados)
+    pet = input("Informe o nome do pet que voce quer remover: ") 
+    delete_script = f"delete from adocao where nome = '{pet}'; "
+    cur.execute(delete_script)
+    conn.commit()   
+
+def imprime_estoque(dados):
+    for i in dados:
+        print(f"Produto: {i[0]}  \t quantidade: {i[1]} \t valor: {i[2]}")
+
+def verifica_adocao():
+    select_script = f"select * from adocao order by nome; "
+    cur.execute(select_script)
+    dados = cur.fetchall()
+    return dados
+
+def imprime_adocao(dados):
+    for i in dados:
+        print(f"Nome: {i[0]}  \t Especie: {i[1]}")
+
+def inserir_consulta(emailCliente,nomeTutor, nomePet, raca, dataConsulta,horario,motivo,email):
+
+    insert_scrip = f"INSERT INTO consulta values ('{emailCliente}','{nomeTutor}','{nomePet}','{raca}','{dataConsulta}','{horario}','{motivo}','{email}');"
+    cur.execute(insert_scrip)
+    conn.commit()
+
+def marcar_consulta():
+    email = input("Informe seu email: ")
+    nomeTutor = input("Informe seu nome: ")
+    nomePet = input("Informe o nome do seu pet: ")
+    raca = input("Informe a raca do seu pet: ")
+    dataConsulta = input("Informe a data que voce deseja: ")
+    horario = input("Informe o horario da consulta: ")
+    motivo = input("Informe o motivo da consulta: ")
+
+    select_script = f"select * from veterinario order by nome; "
+    cur.execute(select_script)
+    dados = cur.fetchall()
+    for i in dados:
+        print(f"Nome: {i[0]} \t Email: {i[1]}")
+    emailVet = input("Esses são os veterinarios disponiveis, escolha um deles e informe o email: ")
+
+    inserir_consulta(email,nomeTutor,nomePet,raca,dataConsulta,horario,motivo,emailVet)
+
 
 def novo_login():
     
